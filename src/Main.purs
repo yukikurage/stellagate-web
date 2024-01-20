2ab6d5ccaca8e98d9d73672a7269b616e9fe5e2f
yukikurage <55534323+yukikurage@users.noreply.github.com>
2024-01-20 22:28:41 +0900

    first commit

---
+++ src/Main.purs
@@ -0,0 +1,24 @@
+module Main where
+
+import Prelude
+
+import Data.Foldable (traverse_)
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Jelly.Aff (awaitBody)
+import Jelly.Component (Component, text)
+import Jelly.Element as JE
+import Jelly.Hooks (runHooks_)
+import Jelly.Hydrate (mount)
+import Jelly.Prop ((:=))
+
+main :: Effect Unit
+main = launchAff_ do
+  bodyMaybe <- awaitBody
+  liftEffect $ traverse_ (runHooks_ <<< mount component) bodyMaybe
+
+component :: forall m. Component m
+component =
+  JE.div [ "class" := "w-screen h-screen text-5xl flex justify-center items-center" ] do
+    text "Hello Jelly!"
